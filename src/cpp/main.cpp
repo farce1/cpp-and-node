f0ebbb6aadbdf39e633d88e7ea542c27bad85709
Grzegorz Sztuczyński <sztuczynski.grzegorz@gmail.com>
2020-11-07 23:27:36 +0100

    Check prime numbers cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,20 +1,30 @@
 #include <nan.h>
 
-// NAN_METHOD is a Nan macro enabling convenient way of creating native node functions.
-// It takes a method's name as a param. By C++ convention, I used the Capital cased name.
-NAN_METHOD(Hello) {
-    // Create an instance of V8's String type
-    auto message = Nan::New("Hello from C++!").ToLocalChecked();
-    // 'info' is a macro's "implicit" parameter - it's a bridge object between C++ and JavaScript runtimes
-    // You would use info to both extract the parameters passed to a function as well as set the return value.
-    info.GetReturnValue().Set(message);
+NAN_METHOD(IsPrime) {
+    if (!info[0]->IsNumber()) {
+        Nan::ThrowTypeError("argument must be a number!");
+        return;
+    }
+    
+    int number = (int) info[0]->NumberValue();
+    
+    if (number < 2) {
+        info.GetReturnValue().Set(Nan::False());
+        return;
+    }
+    
+    for (int i = 2; i < number; i++) {
+        if (number % i == 0) {
+            info.GetReturnValue().Set(Nan::False());
+            return;
+        }
+    }
+    
+    info.GetReturnValue().Set(Nan::True());
 }
 
-// Module initialization logic
 NAN_MODULE_INIT(Initialize) {
-    // Export the `Hello` function (equivalent to `export function Hello (...)` in JS)
-    NAN_EXPORT(target, Hello);
+    NAN_EXPORT(target, IsPrime);
 }
 
-// Create the module called "addon" and initialize it with `Initialize` function (created with NAN_MODULE_INIT macro)
 NODE_MODULE(addon, Initialize);
